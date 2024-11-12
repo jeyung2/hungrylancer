7d39c3bca2d38ba7d31c3c40f28bda5c1ab8923e
Park Jeyoung <jeyung2@gmail.com>
2024-11-12 15:28:36 +0900

    addd

--- server.js
+++ server.js
@@ -10,4 +10,33 @@ app.get("/api/hello", (req, res) => {
   res.send({ message: "Hello Express!" });
 });
 
+app.get("/api/customers", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      image: "https://picsum.photos/200/300?random=1",
+      name: "홍길동1",
+      birthday: "123456",
+      gender: "남자",
+      job: "대학생",
+    },
+    {
+      id: 2,
+      image: "https://picsum.photos/200/300?random=2",
+      name: "박제영1",
+      birthday: "711123",
+      gender: "남자",
+      job: "프리랜서",
+    },
+    {
+      id: 3,
+      image: "https://picsum.photos/200/300?random=3",
+      name: "박현지1",
+      birthday: "111221",
+      gender: "여자",
+      job: "중학생",
+    },
+  ]);
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
